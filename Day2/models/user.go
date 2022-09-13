0d62b9723373dbcaa7858bb9772d9483bb5be0d7
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-09-13 21:13:40 +0700

    add: models

---
+++ Day2/models/user.go
@@ -0,0 +1,8 @@
+package models
+
+type User struct {
+	// gorm.Model
+	Name     string `json:"name" form:"name"`
+	Email    string `json:"email" form:"email"`
+	Password string `json:"password" form:"password"`
+}
